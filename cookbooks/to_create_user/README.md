b888b799a104c424f10e6227bf246ef23193f5b8
root <root@ip-172-31-11-199.ap-south-1.compute.internal>
2017-08-20 00:56:58 -0400

    First version

---
+++ cookbooks/to_create_user/README.md
@@ -0,0 +1,4 @@
+# to_create_user
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/to_create_user/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook:: to_create_user
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+user 'nkardile'
+user 'seconduser'
